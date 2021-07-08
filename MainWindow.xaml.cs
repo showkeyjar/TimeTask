b04ced2cccec6ad017253691a7124dc099749ebe
kejia.shao <kejia.shao@moji.com>
2021-07-08 17:01:50 +0800

    add setting

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
 using System.IO;
 using System.Linq;
@@ -20,38 +21,42 @@ namespace TimeTask
             {
                 return null;
             }
+            int parseScore = 0;
             var allLines = File.ReadAllLines(filepath).Where(arg => !string.IsNullOrWhiteSpace(arg));
             var result =
                 from line in allLines.Skip(1).Take(allLines.Count() - 1)
                 let temparry = line.Split(',')
                 let isSkip = temparry.Length > 2 && temparry[2] != null && temparry[2] == "True"
-                select new ItemGrid { ItemName = temparry[0], ItemScore = temparry[1], ItemValue = !isSkip };
+                let parse = int.TryParse(temparry[1],out parseScore)
+                select new ItemGrid { Task = temparry[0], Score = parseScore, Done = !isSkip };
             var result_list = new List<ItemGrid>();
             try
             {
                 result_list = result.ToList();
             }
             catch {
-                result_list.Add(new ItemGrid { ItemName = "csv文件缺失", ItemScore = "0", ItemValue = false });
+                result_list.Add(new ItemGrid { Task = "csv文件缺失", Score = parseScore, Result= "", Done = false });
             }
             return result_list;
         }
 
         public static void WriteCsv(IEnumerable<ItemGrid> items, string filepath)
         {
-            var temparray = items.Select(item => item.ItemName + "," + item.ItemScore + "," + (item.ItemValue ? "False" : "True")).ToArray();
+            var temparray = items.Select(item => item.Task + "," + item.Score + "," + item.Result + "," + (item.Done ? "False" : "True")).ToArray();
             var contents = new string[temparray.Length + 2];
             Array.Copy(temparray, 0, contents, 1, temparray.Length);
-            contents[0] = "txt,score,finish";
+            contents[0] = "task,score,result,done";
             File.WriteAllLines(filepath, contents);
         }
     }
 
     public class ItemGrid
     {
-        public string ItemName { set; get; }
-        public string ItemScore { set; get; }
-        public bool ItemValue { set; get; }
+        public string Task { set; get; }
+        public int Score { set; get; }
+        public string Result { set; get; }
+        public bool Done { set; get; }
+
     }
 
     /// <summary>
@@ -78,10 +83,11 @@ namespace TimeTask
             task4.ItemsSource = HelperClass.ReadCsv(@"data/4.csv");
         }
 
-
         public MainWindow()
         {
             InitializeComponent();
+            this.Top = (double)Properties.Settings.Default.Top;
+            this.Left = (double)Properties.Settings.Default.Left;
             loadDataGridView();
         }
 
@@ -129,25 +135,6 @@ namespace TimeTask
             HelperClass.WriteCsv(temp, @"data/4.csv");
         }
 
-        public void SetFormOnDesktop(IntPtr hwnd)
-        {
-            IntPtr hwndf = hwnd;
-            IntPtr hwndParent = FindWindow("ProgMan", null);
-            SetParent(hwndf, hwndParent);
-        }
-
-        public void pinToDesktop() {
-            var handle = new WindowInteropHelper(Application.Current.MainWindow).Handle;
-            IntPtr hprog = FindWindowEx(
-                FindWindowEx(
-                    FindWindow("Progman", "Program Manager"),
-                    IntPtr.Zero, "SHELLDLL_DefView", ""
-                ),
-                IntPtr.Zero, "SysListView32", "FolderView"
-            );
-            SetWindowLong(handle, GWL_HWNDPARENT, hprog);
-        }
-
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             base.OnMouseLeftButtonDown(e);
@@ -161,7 +148,13 @@ namespace TimeTask
             pWnd = FindWindowEx(pWnd, IntPtr.Zero, "SysListView32", null);
             IntPtr tWnd = new WindowInteropHelper(this).Handle;
             SetParent(tWnd, pWnd);
-            //pinToDesktop();
+        }
+
+        private void location_Save(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Top = this.Top;
+            Properties.Settings.Default.Left = this.Left;
+            Properties.Settings.Default.Save();
         }
     }
 }
